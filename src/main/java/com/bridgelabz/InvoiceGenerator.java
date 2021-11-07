c3c942a85ac0f1fad163165fb2b292aba0e524df
lokeshmk25 <lokeshmk2211@gmail.com>
2021-11-07 15:34:35 +0530

    Added: Passes Test cases for given less distance and time should return minimum fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -3,8 +3,13 @@ package com.bridgelabz;
 public class InvoiceGenerator {
     private static final double MIN_COST_PER_KM = 10.0;
     private static final int COST_PER_MIN = 1;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance*MIN_COST_PER_KM + time * COST_PER_MIN;
+        double totalFare= distance*MIN_COST_PER_KM + time * COST_PER_MIN;
+        if(totalFare<MINIMUM_FARE)
+            return MINIMUM_FARE;
+        else
+            return totalFare;
     }
 }
