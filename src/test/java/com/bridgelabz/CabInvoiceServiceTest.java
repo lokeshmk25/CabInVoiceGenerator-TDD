3f0697396a3c9d2bc387c362fa85c1173398f55b
lokeshmk25 <lokeshmk2211@gmail.com>
2021-11-07 14:53:08 +0530

    Added: Failed Test cases for calculating total fare

---
+++ src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CabInvoiceServiceTest {
+
+    @Test
+    void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time=5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25,fare);
+    }
+}
--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class CabInvoiceTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cabInvoice Generator");
-    }
-}
