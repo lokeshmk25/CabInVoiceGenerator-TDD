6cb95efee207f574494dc314d18e04796f6354cf
lokeshmk25 <lokeshmk2211@gmail.com>
2021-11-07 16:21:59 +0530

    Optimized: Existing code

--- src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
@@ -1,35 +1,41 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CabInvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @BeforeEach
+    void setUp() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
 
     @Test
     void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
-        int time=5;
+        int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assertions.assertEquals(25,fare);
+        Assertions.assertEquals(25, fare);
     }
 
     @Test
     void givenLessDistanceandTime_ShouldReturnMinimumFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
-        int time=1;
+        int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assertions.assertEquals(5,fare);
+        Assertions.assertEquals(5, fare);
     }
 
     @Test
     void givenMultipleRides_shouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {new Ride(2.0, 5),
-            new Ride(0.1,1),
-            };
+                new Ride(0.1, 1),
+        };
         double fare = invoiceGenerator.calculateFare(rides);
-        Assertions.assertEquals(30,fare);
+        Assertions.assertEquals(30, fare);
     }
 }
