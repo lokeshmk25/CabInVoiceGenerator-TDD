bd68c40aa30bc0bd009721fd6353d2d8a3e8fcac
lokeshmk25 <lokeshmk2211@gmail.com>
2021-11-07 15:00:28 +0530

    Added: Passed Test cases for calculating total fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,7 +1,10 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
+    private static final double MIN_COST_PER_KM = 10.0;
+    private static final int COST_PER_MIN = 1;
+
     public double calculateFare(double distance, int time) {
-        return 0;
+        return distance*MIN_COST_PER_KM + time * COST_PER_MIN;
     }
 }
